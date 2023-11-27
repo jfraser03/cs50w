a4c06a2d8ec3870caf9a994a6a12adfdd53ac530
jfraser03 <jakebfraser@gmail.com>
2023-11-27 14:59:01 -0500

    Began work on new page feature

--- web50/week3/pset2/wiki/encyclopedia/views.py
+++ web50/week3/pset2/wiki/encyclopedia/views.py
@@ -8,25 +8,27 @@ from . import util
 
 markdown = Markdown()
 
-def handle_post(f):
+def handle_post_search(f):
     def wrapper (request, *args, **kwargs):
         if request.method == "POST":
             search = request.POST.get('q')
-            if search in util.list_entries():
+            if not search:
+                return
+            elif search in util.list_entries():
                 return HttpResponseRedirect(reverse("entry", args=[search]))
             else:
                 return HttpResponseRedirect(reverse("search") + f"?q={search}")
         return f(request, *args, **kwargs)
     return wrapper
 
-@handle_post
+@handle_post_search
 def index(request):
     if request.method == "GET":
         return render(request, "encyclopedia/index.html", {
             "entries": util.list_entries()
         })
 
-@handle_post
+@handle_post_search
 def info(request, entry):
     if request.method == "GET":
         entry_content = util.get_entry(entry)
@@ -41,7 +43,7 @@ def info(request, entry):
             "entry_content": entry_content
         })
     
-@handle_post
+@handle_post_search
 def search(request):
     if request.method == "GET":
         search_term = request.GET.get('q')
@@ -50,11 +52,15 @@ def search(request):
         close_entries = []
 
         for entry in entries:
-            l_entry = entry.lower()
-            if search_term.lower() in l_entry.lower():
+            if search_term.lower() in entry.lower():
                 close_entries.append(entry)
 
         return render(request, "encyclopedia/search.html", {
             "entries": close_entries,
             "term": search_term
         })
+    
+@handle_post_search
+def new(request):
+    if request.method == "GET":
+        return render(request, "encyclopedia/new.html")
