df60b281354610378def40487d9da019afb3cbd1
jfraser03 <jakebfraser@gmail.com>
2023-11-27 12:46:49 -0500

    Completed first 2 tasks

--- web50/week3/pset2/wiki/encyclopedia/views.py
+++ web50/week3/pset2/wiki/encyclopedia/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from markdown2 import Markdown
 
 from . import util
+
+markdown = Markdown()
 
 
 def index(request):
@@ -8,3 +12,22 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def css(request):
+    entry = markdown.convert(util.get_entry('css'))
+
+    return render(request, "encyclopedia/css_test.html", {
+        "entries": entry
+    })
+
+def info(request, entry):
+    entry_content = util.get_entry(entry)
+
+    if not entry_content:
+        raise Http404("Entry Not Found.")
+    else:
+        entry_content = markdown.convert(entry_content)
+
+    return render(request, "encyclopedia/entry.html", {
+        "entry": entry,
+        "entry_content": entry_content
+    })
