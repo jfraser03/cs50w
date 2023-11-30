4f73dbefc2abcaa558e49237ac59892212e6b822
jfraser03 <jakebfraser@gmail.com>
2023-11-30 15:26:27 -0500

    Added database/Model structure

--- web50/week4/commerce/auctions/admin.py
+++ web50/week4/commerce/auctions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Listing, Category, Comment, Watchlist, ListCat, Bid
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Listing)
+admin.site.register(Category)
+admin.site.register(ListCat)
--- web50/week4/commerce/auctions/models.py
+++ web50/week4/commerce/auctions/models.py
@@ -3,4 +3,55 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    class Meta:
+        db_table = 'users'
+
+# ENTITY tables below
+
+class Listing(models.Model):
+    title = models.CharField(max_length=40)
+    description = models.CharField(max_length=280)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    photo = models.ImageField(upload_to='listing_photos', null=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+    
+class Category(models.Model):
+    title = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.title
+    
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    content = models.CharField(max_length=280)
+
+    def __str__(self):
+        return f"{self.user.username}'s comment on {self.listing.title}"
+
+# RELATIONAL tables below
+
+class Watchlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} watching {self.listing.title}"
+
+class ListCat(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Listing: {self.listing.title} | Category: {self.category.title}"
+
+class Bid(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.user.username} | Bid: {self.listing.title} | Amount: {self.price}"
