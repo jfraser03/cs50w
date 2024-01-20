f6dbeef148c37b000cddbec779c9dc21a45ad9d6
jfraser03 <jakebfraser@gmail.com>
2024-01-19 20:55:48 -0500

    Began model creation

--- web50/week6/project4/network/models.py
+++ web50/week6/project4/network/models.py
@@ -4,3 +4,16 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=280)
+    datetime = models.DateTimeField()
+
+class Follow(models.Model):
+    follower = models.ForeignKey(User, on_delete=models.CASCADE)
+    following = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
